44d6f8180e38c913d2bb702123c24003152c0dc2
Elcin500 <elcinha@code.edu.az>
2020-06-09 18:25:50 +0400

    as

--- Promedi-project/assets/js/index.js
+++ Promedi-project/assets/js/index.js
@@ -1,7 +1,44 @@
 $(document).ready(function () {
-    
 
-    $(".site-header").load("_header.html");
+
+    $(".site-header").load("_header.html",test);
     $(".site-footer").load("_footer.html");
 
+
+ 
+
 });
+
+function test(){
+    //  let li = $('#li');
+    let hidden = $('#hidden');
+
+    // $(li).hover(function () {
+    //         // over
+    //         $(hidden).removeClass('none');
+    //     }, function () {
+    //         // out
+    //         $(hidden).addClass('none');
+    //     }
+    // );
+
+    let lis=$('.ul li');
+
+    $.each(lis, function (queue, element) { 
+         
+            $(element).hover(function () {
+            // over
+            $(hidden).removeClass('none');           ////////////////qaldigim yer hiddeni de dovre sal sora hoveri yoxla
+        }, function () {
+            // out
+            $(hidden).addClass('none');
+        }
+    );
+
+    });
+
+
+
+    console.log(lis);
+
+}
