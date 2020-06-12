d50563b54c2ba394a03b27f8db780bc91cb4cdbd
Elcin500 <elcinha@code.edu.az>
2020-06-12 23:32:35 +0400

    accept footer-start

--- Promedi-project/assets/js/index.js
+++ Promedi-project/assets/js/index.js
@@ -1,15 +1,32 @@
 $(document).ready(function () {
 
 
-    $(".site-header").load("_header.html", test);
-    $(".site-footer").load("_footer.html");
+    $(".site-header").load("_header.html", header);
+    $(".site-footer").load("_footer.html",footer);
 
 
+    
 
+$(document).scroll(function () { 
+  console.log($(document).scrollTop());
+
+  if($(document).scrollTop() >= 155)
+  {
+     $('.header-bottom').removeClass('scroll');
+    $('.header-bottom').addClass('fixed-top');
+     
+  }
+  else{
+    $('.header-bottom').addClass('scroll');
+    $('.header-bottom').removeClass('fixed-top');
+
+  }
+  
+});
 
 });
 
-function test() {
+function header() {
     let lis = $('.ul li');
     $(lis).each(function (index, element) {
         // element == this
@@ -33,7 +50,15 @@ function test() {
             $(hidden).addClass('none');
         });
 
+       
+
     });
     // console.log(hidden);
 
+ 
+}
+
+
+function footer() {
+    
 }
