c86af318dfac2d37cce9fa001f519ecd4e2763d5
Elcin500 <elcinha@code.edu.az>
2020-06-15 18:06:27 +0400

    DOctors with ajax is nearly over

--- Promedi-project/assets/js/index.js
+++ Promedi-project/assets/js/index.js
@@ -1,9 +1,92 @@
 $(document).ready(function () {
 
 
+// header and footer 
     $(".site-header").load("_header.html", header);
     $(".site-footer").load("_footer.html",footer);    
 
+
+
+
+    // Doctor slider ajax 
+// First doctor
+    $.ajax({
+        type: "GET",
+        url: "http://kamranaeff1994-002-site3.ctempurl.com/api/Doctors/2",
+        success: function (response) {
+          console.log(response.image);
+            $('.item-1 h3').text(response.name);
+            $('.item-1 span').text(response.profession);
+            $('.item-1 p').text(response.description);
+
+        },
+        error: function (response) {
+            console.log('Error');
+            
+        }
+    });
+
+    // Second doctor
+    $.ajax({
+        type: "GET",
+        url: "http://kamranaeff1994-002-site3.ctempurl.com/api/Doctors/3",
+        success: function (response) {
+           $('.item-2 h3').text(response.name);
+           $('.item-2 span').text(response.profession);
+           $('.item-2 p').text(response.description);
+           
+        },
+        error: function (response) {
+            console.log('Error');
+            
+        }
+    });
+
+    // Third doctor
+    $.ajax({
+        type: "GET",
+        url: "http://kamranaeff1994-002-site3.ctempurl.com/api/Doctors/4",
+        success: function (response) {
+
+            $('.item-3 h3').text(response.name);
+            $('.item-3 span').text(response.profession);
+            $('.item-3 p').text(response.description);
+        },
+        error: function (response) {
+            console.log('Error');
+            
+        }
+    });
+
+    // Fifth doctor
+    $.ajax({
+        type: "GET",
+        url: "http://kamranaeff1994-002-site3.ctempurl.com/api/Doctors/5",
+        success: function (response) {
+            $('.item-4 h3').text(response.name);
+            $('.item-4 span').text(response.profession);
+            $('.item-4 p').text(response.description);
+        },
+        error: function (response) {
+            console.log('Error');
+            
+        }
+    });
+
+
+
+//counter up
+    
+    $('.counter').counterUp(
+        {  delay:10,
+          time:2000
+          
+      }
+      );
+
+
+
+    //  scroll
 $(document).scroll(function () { 
 
   if($(document).scrollTop() >= 155)
@@ -36,7 +119,9 @@ $(document).scroll(function () {
 
 });
 
-$('.owl-carousel').owlCarousel({
+
+// Photo carousel
+$('.photo-carousel').owlCarousel({
     loop:true,
     nav:true,   
     dots:false,
@@ -55,9 +140,33 @@ $('.owl-carousel').owlCarousel({
 })
 
 
+// doctors carousel
+$('.doctor-carousel').owlCarousel({
+    loop:true,
+    nav:true,   
+    dots:false,
+    margin:30,
+    // autoplay:true,
+    // autoplayTimeout:3000,
+    // autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600 :{
+           items:2
+        },
+        1000:{
+            items:3
+        }
+    },
+    navText : ["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"]
+})
 
 });
 
+
+//Header functions
 function header() {
     let lis = $('.ul li');
     $(lis).each(function (index, element) {
@@ -90,13 +199,13 @@ function header() {
  
 }
 
-
+// Footer
 function footer() {
     
 }
 
  
-
+// Call ac tion aid kid hover
 $('.call-action').hover(function () {
     // over
     $('.call-action-text i').css({
